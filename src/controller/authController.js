e48216b5cb7d76cd7c25aba74595ab5483790a0f
Comelandio <55109542+Comelandio@users.noreply.github.com>
2020-10-01 21:45:10 -0300

    day

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -5,8 +5,14 @@ const User = require('../model/User');
 const router = express.Router();
 
 router.post('/register', async (request, response) => {
+    const { email } = request.body;
+    
     try {
+      if (await User.findOne({ email }))
+      { return response.status(400).send({ error: "User already exist" })}
+
         const user = await User.create(require.body);
+        User.password = undefined;
 
         return response.send({ user });
     } catch (error) {
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,10 @@
-const mongoose = require('../database');
+const mongoose = require('mongoose');
 
-mongoose.connect('mongoose://Localhost/noderest', { useMongoClient: true });
+//mongoose.connect('mongoose://Localhost/noderest', { useMongoClient: true });
+mongoose.connect('mongodb://localhost:27017/noderest', 
+    { useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
