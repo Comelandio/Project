0b3fc848040e3c45212da56b4aab5beed0961f54
Comelandio <55109542+Comelandio@users.noreply.github.com>
2020-10-12 22:05:42 -0300

    cc

--- src/database/migrations/00_createUser.js
+++ src/database/migrations/00_createUser.js
@@ -10,7 +10,7 @@ export function up (queryInterface, Sequelize) {
             type: Sequelize.INTEGER,
             alloNull: false,
             autoIncremento: true,
-            //primarykey: true
+            primarykey: true
         },
         name: {
             type: Sequelize.STRING,
